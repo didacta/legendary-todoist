0eacd340c3ba7b529a873a37e8316098eb0fd0a5
didacta <igor.yermak@gmail.com>
2019-07-22 21:06:03 -0400

    firebase config

---
+++ todoist/src/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+firebaseConfig = firebase.initalizeApp({
+  apiKey: "AIzaSyCOXCp0klj0757jG2KhdH_TNUVZM2VUVYE",
+  authDomain: "legendary-todoist.firebaseapp.com",
+  databaseURL: "https://legendary-todoist.firebaseio.com",
+  projectId: "legendary-todoist",
+  storageBucket: "legendary-todoist.appspot.com",
+  messagingSenderId: "822239534445",
+  appId: "1:822239534445:web:31f7826611b21d3c"
+});
+
+export { firebaseConfig as firebase };
