7b18789d66fb66031f37e27d3eebaf3dae479b8d
didacta <igor.yermak@gmail.com>
2019-07-29 16:11:11 -0400

    css changes

--- todoist/src/components/Checkbox.js
+++ todoist/src/components/Checkbox.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import { firebase } from "../Firebase";
+import { firebase } from "../firebase";
 
 export const Checkbox = ({ id }) => {
   const archiveTask = () => {
     firebase
-    .firestore()
-    .collection('tasks')
-    .doc(id)
-    .update({
+      .firestore()
+      .collection("tasks")
+      .doc(id)
+      .update({
         archived: true
-      })
-    
+      });
   };
 
   return (
--- todoist/src/components/Tasks.js
+++ todoist/src/components/Tasks.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Checkbox } from "./Checkbox";
 import { useTasks } from "../hooks";
+
+import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
+import { collatedTasks } from "../constants";
 
 export const Tasks = () => {
   const { tasks } = useTasks('1');
@@ -11,6 +14,7 @@ export const Tasks = () => {
   return (
     <div className="tasks" data-testid="tasks">
       <h2 data-testid="project-name">{projectName}</h2>
+
       <ul className="tasks__list">
         {tasks.map(task => (
           <li key={`${task.id}`}>
