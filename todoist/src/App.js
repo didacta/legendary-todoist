29031980dc68383b97c7f79813a40b9c04b0282e
didacta <igor.yermak@gmail.com>
2019-08-03 17:31:35 -0400

    selected project provider  context

--- todoist/src/App.js
+++ todoist/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import {Header} from './components/layout/Header';
-import {Content} from './components/layout/Content';
-
+import React from "react";
+import { Header } from "./components/layout/Header";
+import { Content } from "./components/layout/Content";
+import { ProjectsProvider, selectedProjectProvider } from "./context";
 export const App = () => {
   return (
-    <div className="App">
-    <Header/>
-    <Content/>
-    </div>
+    <selectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header />
+          <Content />
+        </div>
+      </ProjectsProvider>
+    </selectedProjectProvider>
   );
-}
+};
