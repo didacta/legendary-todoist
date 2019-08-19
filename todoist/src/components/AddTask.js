0bfa3c941612e364b1c380b51d71936b1fc4f145
didacta <igor.yermak@gmail.com>
2019-08-18 22:26:59 -0400

    task date component

--- todoist/src/components/AddTask.js
+++ todoist/src/components/AddTask.js
@@ -4,7 +4,7 @@ import moment from "moment";
 import { firebase } from "../firebase";
 import { useSelectedProjectValue } from "../context";
 import { ProjectOverlay } from "./ProjectOverlay";
-
+import { TaskDate} from "./TaskDate"
 export const AddTask = ({
   showAddTaskMain = true,
   shouldShowMain = false,
@@ -93,7 +93,11 @@ export const AddTask = ({
             showProjectOverlay={showProjectOverlay}
             setShowProjectOverlay={setShowProjectOverlay}
           />
-          <p>TaskDate here</p>
+          <TaskDate
+            setTaskDate={setTaskDate}
+            showTaskDate={showTaskDate}
+            setShowTaskDate={setShowTaskDate}
+          />
           <input
             className="add-task__content"
             data-testid="add-task-content"
